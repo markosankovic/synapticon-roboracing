7d1b56efa2b330a45203544acd62a18d2093962c
Marko Sanković <sankovic.marko@gmail.com>
2017-05-10 10:00:50 +0200

    Add components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,50 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { HighScoresComponent } from './high-scores/high-scores.component';
+import { NewRaceComponent } from './new-race/new-race.component';
+import { CountdownComponent } from './countdown/countdown.component';
+import { RaceComponent } from './race/race.component';
+import { FinishComponent } from './finish/finish.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HighScoresComponent,
+    NewRaceComponent,
+    CountdownComponent,
+    RaceComponent,
+    FinishComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot([
+      {
+        path: 'high-scores',
+        component: HighScoresComponent
+      },
+      {
+        path: 'new-race',
+        component: NewRaceComponent
+      },
+      {
+        path: 'countdown',
+        component: CountdownComponent
+      },
+      {
+        path: 'race',
+        component: RaceComponent
+      },
+      {
+        path: 'finish',
+        component: FinishComponent
+      }
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/new-race/new-race.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-new-race',
+  templateUrl: './new-race.component.html',
+  styleUrls: ['./new-race.component.css']
+})
+export class NewRaceComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
