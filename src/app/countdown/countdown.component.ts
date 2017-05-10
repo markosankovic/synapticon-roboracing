d59c3b5d03c005a0d77882fa0bd17b4dd84c1e61
Marko Sanković <sankovic.marko@gmail.com>
2017-05-10 10:29:07 +0200

    Implement countdown

--- src/app/countdown/countdown.component.ts
+++ src/app/countdown/countdown.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs/Rx';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-countdown',
@@ -7,9 +9,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CountdownComponent implements OnInit {
 
-  constructor() { }
+  countdown: number = 3;
+
+  constructor(private router: Router) { }
 
   ngOnInit() {
+    const timer = Observable.timer(1000, 1000);
+    const subscribe = timer.subscribe(() => {
+      if (this.countdown > 0) {
+        --this.countdown;
+      } else {
+        subscribe.unsubscribe();
+        this.router.navigate(['/race']);
+      }
+    });
   }
-
 }
